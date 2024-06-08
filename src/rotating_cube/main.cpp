8277c8525eacad08835afc6551183e4dd65c0250
bluemoon <flevn@protonmail.com>
2024-06-08 12:27:12 +0200

    Fixed cube winding order

--- src/rotating_cube/main.cpp
+++ src/rotating_cube/main.cpp
@@ -245,12 +245,12 @@ Shader loadWallsShader(char const* vertpath,
   }
   float constexpr vertices[] = {
     -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom-left
-     0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom-right
-    -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, // top-left
-
-    -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, // top-left
      0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top-right
      0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom-right
+
+     0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top-right
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom-left
+    -0.5f,  0.5f, 0.0f, 0.0f, 1.0f, // top-left
   };
   unsigned int VAO, VBO;
   glGenBuffers(1, &VBO);
